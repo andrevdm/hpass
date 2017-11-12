028c0a6d8ffc6e78676ae526ecd254dc9cc7eecb
Andre Van Der Merwe <andre@andrevdm.com>
2017-11-12 16:34:50 +0200

    Focus ring

--- src/UI.hs
+++ src/UI.hs
@@ -9,6 +9,7 @@ import qualified Data.Vector as Vec
 import           Brick ((<+>))
 import qualified Brick as B
 import qualified Brick.BChan as BCh
+import qualified Brick.Focus as BF
 import qualified Brick.AttrMap as BA
 import qualified Brick.Widgets.List as BL
 import qualified Brick.Widgets.Border as BB
@@ -23,10 +24,16 @@ main = do
   let items = Vec.fromList $ Lib.flattenDirs ps
 
   chan <- BCh.newBChan 10
-  let g = St ps (BL.list ListFolder items 1) (BL.list ListFile Vec.empty 1)
-  void $ B.customMain (V.mkVty V.defaultConfig) (Just chan) app g
 
-data Name = ListFolder
+  let st = St { stPassAll = ps
+              , stListDir = BL.list ListDir items 1
+              , stListFile = BL.list ListFile Vec.empty 1
+              , stFocusRing = BF.focusRing [ListDir, ListFile]
+              }
+          
+  void $ B.customMain (V.mkVty V.defaultConfig) (Just chan) app st
+
+data Name = ListDir
           | ListFile
           deriving (Show, Ord, Eq)
           
@@ -34,6 +41,7 @@ data Event = Event
 data St = St { stPassAll :: Lib.PassDir
              , stListDir :: BL.List Name Lib.PassDir
              , stListFile :: BL.List Name Lib.PassFile
+             , stFocusRing :: BF.FocusRing Name
              }
 
 app :: B.App St Event Name
@@ -45,41 +53,73 @@ app = B.App { B.appDraw = drawUI
             }
 
 handleEvent :: St -> B.BrickEvent Name Event -> B.EventM Name (B.Next St)
-handleEvent g (B.VtyEvent (V.EvKey V.KEsc [])) = B.halt g
-handleEvent g (B.VtyEvent (V.EvKey (V.KChar 'q') [])) = B.halt g
-handleEvent g (B.VtyEvent e) = do
-  g' <- case e of
-          V.EvKey (V.KChar 'k') [] -> do
-            let r = BL.listMoveBy (-1) $ stListDir g
-            pure g { stListDir = r }
-          V.EvKey (V.KChar 'j') [] -> do
-            let r = BL.listMoveBy 1 $ stListDir g
-            pure g { stListDir = r }
+handleEvent st (B.VtyEvent (V.EvKey V.KEsc [])) = B.halt st
+handleEvent st (B.VtyEvent (V.EvKey (V.KChar 'q') [])) = B.halt st
+handleEvent st (B.VtyEvent e) = do
+  st' <- case BF.focusGetCurrent (stFocusRing st) of
+           Just ListFile ->
+             case e of
+                    V.EvKey (V.KChar 'k') [] -> do
+                      let r = BL.listMoveBy (-1) $ stListFile st
+                      pure st { stListFile = r }
 
-          _ -> do
-            r <- BL.handleListEvent e (stListDir g)
-            pure g { stListDir = r }
+                    V.EvKey (V.KChar 'j') [] -> do
+                      let r = BL.listMoveBy 1 $ stListFile st
+                      pure st { stListFile = r }
 
-  r <- case BL.listSelectedElement (stListDir g') of
-         Nothing ->
-           pure g { stListFile = BL.listClear (stListFile g') }
-         Just (_, dir) -> do
-           let items = Vec.fromList $ Lib.pdFiles dir
-           pure g' { stListFile = BL.list ListFile items 1 }
+                    V.EvKey (V.KChar 'h') [] ->
+                      pure st { stFocusRing = BF.focusPrev . stFocusRing $ st }
+
+                    V.EvKey (V.KChar '\t') [] ->
+                      pure st { stFocusRing = BF.focusPrev . stFocusRing $ st }
+
+                    _ -> do
+                      r <- BL.handleListEvent e (stListFile st)
+                      pure st { stListFile = r }
+
+           Just ListDir -> do
+             st2 <- case e of
+                      V.EvKey (V.KChar 'k') [] -> do
+                        let r = BL.listMoveBy (-1) $ stListDir st
+                        pure st { stListDir = r }
+
+                      V.EvKey (V.KChar 'j') [] -> do
+                        let r = BL.listMoveBy 1 $ stListDir st
+                        pure st { stListDir = r }
+
+                      V.EvKey (V.KChar 'l') [] ->
+                        pure st { stFocusRing = BF.focusNext . stFocusRing $ st }
+
+                      V.EvKey (V.KChar '\t') [] ->
+                        pure st { stFocusRing = BF.focusNext . stFocusRing $ st }
+
+                      _ -> do
+                        r <- BL.handleListEvent e (stListDir st)
+                        pure st { stListDir = r }
+
+             case BL.listSelectedElement (stListDir st2) of
+                Nothing ->
+                  pure st2 { stListFile = BL.listClear (stListFile st2) }
+                Just (_, dir) -> do
+                  let items = Vec.fromList $ Lib.pdFiles dir
+                  pure st2 { stListFile = BL.list ListFile items 1 }
+
+           _ ->
+             pure st
   
-  B.continue r
-handleEvent g _ = B.continue g
+  B.continue st'
+handleEvent st _ = B.continue st
 
 -- Drawing
 
 drawUI :: St -> [B.Widget Name]
-drawUI g =
+drawUI st =
   [ (
       B.hLimit 50 $
       B.withBorderStyle BBS.unicodeRounded $
       BB.borderWithLabel (B.str "folders") $
       B.padAll 1 $
-      BL.renderList listDrawDir True (stListDir g)
+      BF.withFocusRing (stFocusRing st) (BL.renderList listDrawDir) (stListDir st)
     )
     <+>
     (
@@ -87,10 +127,7 @@ drawUI g =
       B.withBorderStyle BBS.unicodeRounded $
       BB.borderWithLabel (B.str "passwords") $
       B.padAll 1 $
-      BL.renderList listDrawFile False (stListFile g)
-    )
-  ]
-
+      BF.withFocusRing (stFocusRing st) (BL.renderList listDrawFile) (stListFile st))] 
 listDrawDir :: Bool -> Lib.PassDir -> B.Widget a
 listDrawDir _ d =
   B.str . Txt.unpack $ Txt.replicate (Lib.pdDepth d) " " <> Lib.pdName d
@@ -104,7 +141,8 @@ customAttr :: BA.AttrName
 customAttr = BL.listSelectedAttr <> "custom"
 
 theMap :: BA.AttrMap
-theMap = BA.attrMap V.defAttr [ (BL.listAttr,         V.white `B.on` V.blue)
-                              , (BL.listSelectedAttr, V.blue `B.on` V.white)
-                              , (customAttr,          B.fg V.cyan)
+theMap = BA.attrMap V.defAttr [ (BL.listAttr               , V.white `B.on` V.blue)
+                              , (BL.listSelectedAttr       , V.blue  `B.on` V.white)
+                              , (BL.listSelectedFocusedAttr, V.black `B.on` V.yellow)
+                              , (customAttr                , B.fg V.cyan)
                               ]
